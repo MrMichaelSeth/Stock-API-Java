194b238174fc5c6b8fc5e6e69342b123336a45f8
Michael Seth <michael.a.seth175@gmail.com>
2023-02-09 23:48:05 -0500

    Added Http GET request and displaying the response

---
+++ src/main/java/Info.java
@@ -0,0 +1,20 @@
+public class Info {
+
+    private String symbol;
+    private String metaData;
+    public String getMetaData() {
+        return metaData;
+    }
+
+    public void setMetaData(String metaData) {
+        this.metaData = metaData;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
-import java.util.*;
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.net.*;
+import java.util.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         //Receive user input
         Scanner input = new Scanner(System.in);
@@ -13,7 +22,7 @@ public class Main {
     }
 
     //See if they inputted a ticker or want to exit
-    public static void continueOrExit(String ticker) {
+    public static void continueOrExit(String ticker) throws IOException, InterruptedException {
         if (ticker.equals("EXIT")) {
             //Exit sequence
             System.out.println("Thank you! Goodbye!");
@@ -23,8 +32,33 @@ public class Main {
         }
     }
 
-    public static void apiCAll(String ticker) {
+    public static void apiCAll(String ticker) throws IOException, InterruptedException {
        // System.out.println("API call for ticker: " + ticker);
 
-            }
+        String apiKey = "HOQ79Q7MW5T7G7PR";
+
+        var uri = URI.create("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + ticker + "&interval=5min&outputsize=compact&apikey=" + apiKey);
+
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .timeout(Duration.ofSeconds(10))
+                .build();
+
+        HttpResponse<String> getResponse = client.send (
+                request,
+                HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8)
+        );
+
+        System.out.println(getResponse.body());
+
+//        var response = getResponse.body();
+//        Gson gson = new Gson();
+//
+//        Info info = gson.fromJson(getResponse.body(), Info.class);
+
+
         }
+    }
